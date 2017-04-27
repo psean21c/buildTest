605e13fbafa6f4c2b7f937b0611b9d8963859b6e
Simon Park <psean21c@gmail.com>
2017-04-27 16:07:50 -0400

    Update BuildApp.java

--- BuildApp.java
+++ BuildApp.java
@@ -1,8 +1,6 @@
 
 public class BuildApp {
-	public static void main(String[] args) {
-   System.out.println("Build Application ..");
+  public static void main(String[] args) {
+    System.out.println("Build Application ..If you see this message, it's successful ..");
   }
 }
-
- 
